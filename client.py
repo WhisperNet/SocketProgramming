feeeddaeca7e5d06b4265bc41e5648b4a85e5bb5
Mohammad Ridowan Sikder <83389896+WhisperNet@users.noreply.github.com>
2024-03-07 16:04:12 +0600

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,39 @@
 import socket
+import ipaddress
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MSG ='!dis'
-#################################################
-# Assign the local IP address of the SERVER to the SERVER variable
-SERVER = socket.gethostbyname(socket.gethostname())
-#SERVER = ''
-#################################################
-ADDR = (SERVER,PORT)
 Terminate = 'running'
 Welcome = True
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
 
+def is_valid_local_ip(ip_address):
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        return ip.is_private
+
+    except ValueError:
+        return False
+
+
+while Welcome:
+    SERVER = input("\nEnter the IP address of the SERVER: ")
+    if is_valid_local_ip(SERVER):
+        print(f"Client is trying to connect with the server {SERVER}.....")
+        break
+    else:
+        print(f"{SERVER} is not a valid IP address.")
+
+ADDR = (SERVER,PORT)
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+try:
+    client.connect(ADDR)
+    print("Connection Established\nTo disconnect from the server prompt with !dis")
+except:
+    print("The given IP has refused to connect. Please check if the server.py is running")
+    Terminate = False
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -25,9 +44,6 @@ def send(msg):
     client.send(message)
     print("Server Reply: "+client.recv(2048).decode(FORMAT))
 
-if Welcome:
-    print("     Client is trying to connect with the server.....")
-    print("     Connection Established\n     To disconnect from the server prompt with !dis")
 
 while Terminate:
     INPUT = input("Enter Your Message For the SERVER: ")
